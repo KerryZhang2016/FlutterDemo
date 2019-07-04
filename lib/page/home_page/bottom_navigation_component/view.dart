f83a1153467373f12db0b12f7f8c52530f91f7ec
zhangke <zhangke@tigerbrokers.com>
2019-07-04 10:59:49 +0800

    【UPT】更新底部导航栏的显示样式选择

--- lib/page/home_page/bottom_navigation_component/view.dart
+++ lib/page/home_page/bottom_navigation_component/view.dart
@@ -14,7 +14,6 @@ Widget buildView(CustomBottomNavigationBarState state, Dispatch dispatch,
     child: SizedBox(
       height: 46.0,
       child: Card(
-        color: Platform.isIOS ? Colors.transparent : Colors.white,
         elevation: Platform.isIOS ? 0.0 : 8.0,
         shape: RoundedRectangleBorder(),
         margin: EdgeInsets.all(0.0),
@@ -46,38 +45,48 @@ List<Widget> _buildBottomItems(
 
 Widget _buildBottomItem(
     CustomBottomNavigationBarState state, Dispatch dispatch, int index) {
+  Widget _wrapInk(bool isInk, Widget child, GestureTapCallback onTap) {
+    return isInk
+        ? InkResponse(child: child, onTap: onTap)
+        : GestureDetector(
+            child: Container(
+                child: child,
+                color: Platform.isIOS ? Colors.transparent : Colors.white),
+            onTap: onTap);
+  }
+
+  Widget bottomItem = Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image(
+            image: AssetImage(state.currentIndex == index
+                ? state.tabIconSelectList[index]
+                : state.tabIconList[index]),
+            width: 19.0,
+            height: 19.0,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 3.0),
+            child: Text(state.tabTextList[index],
+                style: TextStyle(
+                    color: (state.currentIndex == index)
+                        ? Color(CustomColor.home_tab_select_text_color)
+                        : Color(CustomColor.home_tab_text_color),
+                    fontSize: CustomDimen.text_size_tiny)),
+          )
+        ],
+      )
+    ],
+  );
+
   return Expanded(
-      child: InkResponse(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              image: AssetImage(state.currentIndex == index
-                  ? state.tabIconSelectList[index]
-                  : state.tabIconList[index]),
-              width: 19.0,
-              height: 19.0,
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 3.0),
-              child: Text(state.tabTextList[index],
-                  style: TextStyle(
-                      color: (state.currentIndex == index)
-                          ? Color(CustomColor.home_tab_select_text_color)
-                          : Color(CustomColor.home_tab_text_color),
-                      fontSize: CustomDimen.text_size_tiny)),
-            )
-          ],
-        )
-      ],
-    ),
-    onTap: () =>
-        dispatch(CustomBottomNavigationBarActionCreator.indexChange(index)),
-  ));
+      child: _wrapInk(false, bottomItem, () {
+    dispatch(CustomBottomNavigationBarActionCreator.indexChange(index));
+  }));
 }
